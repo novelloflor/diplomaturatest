3d256e68f7b113c8ee4c88e2a94f84808cce1eb7
Florencia Novello <novelloflor@gmail.com>
2024-02-10 19:50:43 +0100

    js

---
+++ JavaScript_diplo/JS_PART1_2.js
@@ -0,0 +1,11 @@
+function obtenerMayor(arrValores){
+    var c = arrValores[0];
+    for (var i = 0; i < arrValores.length; i++){
+        if (arrValores[i] > c){
+            c = arrValores[i];
+        }
+    }
+}
+
+var arrValores = [4, 5, 6, 10, 25, 2];
+obtenerMayor(arrValores);

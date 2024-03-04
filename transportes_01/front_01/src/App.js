f06b052cc86687c45ed0e45a35dbc54c241563e5
Florencia Novello <novelloflor@gmail.com>
2024-03-04 10:34:54 +0100

    base

---
+++ transportes_01/front_01/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/layout/Header';
+import Nav from './components/layout/Nav';
+import Footer from './components/layout/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Nav/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;

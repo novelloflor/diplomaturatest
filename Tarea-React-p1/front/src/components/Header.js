868231cc3e099c3d376dbf441770a4212d3552ab
Florencia Novello <novelloflor@gmail.com>
2024-03-05 13:17:57 +0100

    tarea

--- Tarea-React-p1/front/src/components/Header.js
+++ Tarea-React-p1/front/src/components/Header.js
@@ -1,8 +1,11 @@
 import React from "react";
+import '../styles/components/Header.css'
 
 const Header = (props) =>{
     return (
-        <div>This is the Header</div>
+        <div>
+            <h1>This is the h1 Header</h1>
+        </div>
     );
 }
 

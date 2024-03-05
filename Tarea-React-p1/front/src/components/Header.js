a079ed599e0b8b9dc1fc8de9b52dacfbc07cb512
Florencia Novello <novelloflor@gmail.com>
2024-03-05 12:59:49 +0100

    base

---
+++ Tarea-React-p1/front/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = (props) =>{
+    return (
+        <div>This is the Header</div>
+    );
+}
+
+export default Header;
---
+++ Tarea-React-p1/front/src/components/Nav.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Nav = (props) =>{
+    return (
+        <div>This is the Nav</div>
+    );
+}
+
+
+export default Nav;
